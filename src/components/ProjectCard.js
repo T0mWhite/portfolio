579211359f42a87987dc7546ff96eee99e2b33e7
T0mWhite <tbw2.mail@gmail.com>
2022-01-24 21:35:15 -0700

    Form and cards displaying

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,12 +1,15 @@
 import React from "react";
-
+import placeholder from "../assets/images/350x150.png";
 export default function ProjectCard() {
   return (
-    <div className="card" style="width: 18rem;">
-    <img src="./assets/images/GitHub-Mark-64px.png" className="card-img-top" alt="..."/>
-    <div className="card-body">
-      <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <div className="card d-flex flex-column" style={{width: "150px"}}>
+      <img src={placeholder} className="card-img-top" alt="..." />
+      <div className="card-body">
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+      </div>
     </div>
-  </div>
   );
 }
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -3,8 +3,11 @@ import ProjectCard from "../ProjectCard";
 
 export default function Projects() {
   return (
-    <div>
+    <div className="display-flex">
       <h1>Projects Page</h1>
+      <ProjectCard />
+      <ProjectCard />
+      <ProjectCard />
       <ProjectCard />
     </div>
   );
