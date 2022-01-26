8300eab5375b509200490a7140814586bb759e9f
T0mWhite <tbw2.mail@gmail.com>
2022-01-25 20:25:12 -0700

    Styling on Contacts

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,32 +3,32 @@ import React from "react";
 export default function Contact() {
   return (
     <div className="w-50 text-center mx-auto">
-      <form>
-        <div>
+      <form className="needs-validation">
+        <div className="p-2">
           <div className="mb-3 input-group">
             <span class="input-group-text">📧</span>
-            <label for="exampleInputEmail1" className="form-label"></label>
+            <label for="email" className="form-label"></label>
             <input
               type="email"
               className="form-control"
-              id="exampleInputEmail1"
+              id="email"
               aria-describedby="emailHelp"
-              placeholder="Email"
+              placeholder="Enter email"
+              name="email"
             />
           </div>
-            <div className="form-text d-block" id="emailHelp">
-              I'll never share your email with anyone else.
-            </div>
+          <div className="form-text d-block" id="emailHelp">
+            I'll never share your email with anyone else.
+          </div>
         </div>
-        <div className="mb-3">
-          <label for="inputMessage" className="form-label">
-          </label>
+        <div className="mb-3 p-3">
+          <label for="inputMessage" className="form-label"></label>
           <textarea
             className="form-control"
             id="message"
             rows="3"
             aria-describedby="messageHelp"
-            placeholder="Message"
+            placeholder="Enter message"
           />
           <div id="messageHelp" className="form-text">
             Reach out to me with your questions, I'll get back to you as soon as
