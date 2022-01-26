336476119afc002fa75f03e60075e838905e3530
T0mWhite <tbw2.mail@gmail.com>
2022-01-25 20:14:58 -0700

    Styling in Contact

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,39 +2,44 @@ import React from "react";
 
 export default function Contact() {
   return (
-    <form>
-      <div className="mb-3">
-        <label for="exampleInputEmail1" className="form-label">
-          Email address
-        </label>
-        <input
-          type="email"
-          className="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-        />
-        <div id="emailHelp" className="form-text">
-          I'll never share your email with anyone else.
+    <div className="w-50 text-center mx-auto">
+      <form>
+        <div>
+          <div className="mb-3 input-group">
+            <span class="input-group-text">📧</span>
+            <label for="exampleInputEmail1" className="form-label"></label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Email"
+            />
+          </div>
+            <div className="form-text d-block" id="emailHelp">
+              I'll never share your email with anyone else.
+            </div>
         </div>
-      </div>
-      <div className="mb-3">
-        <label for="inputMessage" className="form-label">
-          Message
-        </label>
-        <input
-          type="message"
-          className="form-control"
-          id="Inputmessage"
-          aria-describedby="messageHelp"
-        />
-        <div id="messageHelp" className="form-text">
-          Reach out to me with your questions, I'll get back to you as soon as I can.
+        <div className="mb-3">
+          <label for="inputMessage" className="form-label">
+          </label>
+          <textarea
+            className="form-control"
+            id="message"
+            rows="3"
+            aria-describedby="messageHelp"
+            placeholder="Message"
+          />
+          <div id="messageHelp" className="form-text">
+            Reach out to me with your questions, I'll get back to you as soon as
+            I can.
+          </div>
         </div>
-      </div>
 
-      <button type="submit" className="btn btn-primary">
-        Get in touch!
-      </button>
-    </form>
+        <button type="submit" className="btn btn-primary">
+          Get in touch!
+        </button>
+      </form>
+    </div>
   );
 }
