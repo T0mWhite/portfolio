fd865d4d15fe56d950cb894db98dcda65ec429e3
T0mWhite <tbw2.mail@gmail.com>
2022-01-26 19:36:42 -0700

    Styling of About page

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -5,52 +5,54 @@ import React from "react";
 function NavTabs({ currentPage, handlePageChange }) {
   return (
     <nav className="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
-      <div className="container-fluid">
+      <div className="container">
         <a
-          className="navbar-brand"
+          className="navbar-brand col-md-6"
           href="#about"
           onClick={() => handlePageChange("About")}
         >
           Tommy White
         </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#collapsibleNavbar"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="collapsibleNavbar">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                href="#about"
-                onClick={() => handlePageChange("About")}
-              >
-                About
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                href="#projects"
-                onClick={() => handlePageChange("Projects")}
-              >
-                Projects
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                href="#contact"
-                onClick={() => handlePageChange("Contact")}
-              >
-                Contact
-              </a>
-            </li>
-          </ul>
+        <div className="container col-md-6 justify-content-flex-end">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapsibleNavbar"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  href="#about"
+                  onClick={() => handlePageChange("About")}
+                >
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  href="#projects"
+                  onClick={() => handlePageChange("Projects")}
+                >
+                  Projects
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  href="#contact"
+                  onClick={() => handlePageChange("Contact")}
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
